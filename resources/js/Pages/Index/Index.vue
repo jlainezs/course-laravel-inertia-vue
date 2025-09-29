454ec5c344b610bc680f7e4a3d00b0b9f11e5a3d
Pep Lainez <jlainezs@gmail.com>
2025-09-29 07:27:20 +0200

    27 - Persistent layouts

--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -1,6 +1,6 @@
 <template>
     <div>Index</div>
-    <Link href="/hello">Show Page</Link>
+        <Link href="/hello">Show Page</Link>
     <div>
         The message is {{ message }}
     </div>
@@ -10,5 +10,11 @@
 import { Link } from '@inertiajs/vue3'
 
 const props = defineProps(['message'])
+</script>
+<script>
+import MainLayout from "@/Layouts/MainLayout.vue";
 
+export default {
+    layout: MainLayout
+}
 </script>
