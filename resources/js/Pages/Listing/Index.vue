8b637bebec481aef5f0f08d648736c43d05018cb
Pep Lainez <jlainezs@gmail.com>
2025-10-06 07:30:08 +0200

    56 - Custom UI components and grid responsive design

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,6 +1,7 @@
 
 <template>
-  <div v-for="listing in listings" :key="listing.id">
+  <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
+    <Box v-for="listing in listings" :key="listing.id">
     <div>
       <Link :href="route('listing.show', {listing: listing.id})">
         <ListingAddress :listing="listing" />
@@ -16,12 +17,14 @@
         Delete
       </Link>
     </div>
+  </Box>
   </div>
 </template>
 
 <script setup>
 import { Link } from '@inertiajs/vue3'
 import ListingAddress from "@/Components/ListingAddress.vue";
+import Box from "@/Components/UI/Box.vue";
 
 defineProps({
   listings: Array,
