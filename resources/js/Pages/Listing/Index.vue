d3d47c1ba5f6e1d3145078d435908adc105da957
Pep Lainez <jlainezs@gmail.com>
2025-10-06 07:42:07 +0200

    57 - Listing details (non-prop attributes).

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -4,7 +4,8 @@
     <Box v-for="listing in listings" :key="listing.id">
     <div>
       <Link :href="route('listing.show', {listing: listing.id})">
-        <ListingAddress :listing="listing" />
+        <ListingSpace :listing="listing" class="text-lg" />
+        <ListingAddress :listing="listing" class="text-gray-500" />
       </Link>
     </div>
     <div>
@@ -25,6 +26,7 @@
 import { Link } from '@inertiajs/vue3'
 import ListingAddress from "@/Components/ListingAddress.vue";
 import Box from "@/Components/UI/Box.vue";
+import ListingSpace from "@/Components/UI/ListingSpace.vue";
 
 defineProps({
   listings: Array,
