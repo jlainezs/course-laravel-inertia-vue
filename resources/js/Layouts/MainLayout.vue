ba025496f322a3a3c08f9c77d1bb7caf396663ae
Pep Lainez <jlainezs@gmail.com>
2025-10-06 07:22:58 +0200

    55 - Basic styling

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,11 +1,28 @@
 <template>
-    <Link href="/listing">Listings</Link>&nbsp;
-    <Link href="/listing/create">New listing</Link>&nbsp;
+  <header class="border-b border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-800 w-full">
+    <div class="container mx-auto">
+      <nav class="p-4 flex items-center justify-between">
+        <div class="text-lg">
+          <Link :href="route('listing.index')">Listings</Link>&nbsp;
+        </div>
+        <div class="text-xl text-indigo-600 dark:text-indigo-300 font-bold text-center">
+          <Link :href="route('listing.index')">LaraZillow</Link>&nbsp;
+        </div>
+        <div>
+          <Link :href="route('listing.create')" class="bg-indigo-600 hover:bg-indigo-500 text-white font-medium p-2 rouded-md">
+            + New Listing
+          </Link>
+        </div>
+      </nav>
+    </div>
+  </header>
 
-    <div v-if="flashSuccess" class="success">
+  <main class="container mx-auto p-4">
+    <div v-if="flashSuccess" class="mb-4 border rounded-md shadow-sm border-green-200 dark:border-green-800 bg-green-50 dark:bg-green-900 p-2">
       {{ flashSuccess }}
     </div>
     <slot></slot>
+  </main>
 </template>
 
 <script setup>
@@ -20,10 +37,3 @@ const flashSuccess = computed(() => {
 })
 
 </script>
-
-<style scoped>
-  .success{
-    background-color: green;
-    color: white;
-  }
-</style>
