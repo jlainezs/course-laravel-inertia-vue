37363835e42532d0e6902df91f44d91ad2c2c0b5
Pep Lainez <jlainezs@gmail.com>
2025-10-09 07:23:57 +0200

    71 - Authentication controller and routes.

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    // receives any form
+    public function create()
+    {
+
+    }
+
+    // create the session
+    public function store()
+    {
+
+    }
+
+    // logout
+    public function destroy()
+    {
+
+    }
+}
