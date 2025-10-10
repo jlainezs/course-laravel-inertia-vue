9672e5b7e9c6e1c57584609dda405e22c6b48952
Pep Lainez <jlainezs@gmail.com>
2025-10-10 06:40:18 +0200

    74 - Authentication errors

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -28,7 +28,7 @@ class AuthController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended();
+        return redirect()->intended('/listing');
     }
 
     // logout
