925da408daff5dbfcf3477e8ce2d34ac3bf57491
Pep Lainez <jlainezs@gmail.com>
2025-10-12 11:46:49 +0200

    84 - Form links

--- resources/js/Pages/UserAccount/Create.vue
+++ resources/js/Pages/UserAccount/Create.vue
@@ -23,12 +23,17 @@
       <div class="mt-4">
         <button class="btn-primary w-full" type="submit">Create account</button>
       </div>
+      <div class="mt-2 text-center">
+        <Link :href="route('login')" class="text-sm text-gray-500">
+          Already have an account? Click here
+        </Link>
+      </div>
     </div>
   </form>
 </template>
 
 <script setup>
-import {useForm} from "@inertiajs/vue3";
+import {Link, useForm} from "@inertiajs/vue3";
 
 const form = useForm({
   name: null,
