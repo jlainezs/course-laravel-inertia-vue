67ca159e04a91e846addd232939aac6208f85e77
Pep Lainez <jlainezs@gmail.com>
2025-10-14 08:02:12 +0200

    93 - Fix method signature in `ListingPolicy::view` function. Make listing required argument.

--- app/Policies/ListingPolicy.php
+++ app/Policies/ListingPolicy.php
@@ -19,7 +19,7 @@ class ListingPolicy
     /**
      * Determine whether the user can view the model.
      */
-    public function view(?User $user, ?Listing $listing): bool
+    public function view(?User $user, Listing $listing): bool
     {
         return true;
     }
