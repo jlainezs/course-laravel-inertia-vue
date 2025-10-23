a07bcc800f7579ddc625c2aa601d4f67b24465d0
Pep Lainez <jlainezs@gmail.com>
2025-10-23 06:36:09 +0200

    124 - Image upload (Backend)

--- resources/js/Pages/Realtor/ListingImage/Create.vue
+++ resources/js/Pages/Realtor/ListingImage/Create.vue
@@ -1,6 +1,6 @@
 <template>
-<form method="POST" :action="route('realtor.listing.image.store', {listing: listing.id})">
-  <input type="file" multiple name="files" />
+<form method="POST" :action="route('realtor.listing.image.store', {listing: listing.id})" enctype="multipart/form-data">
+  <input type="file"  multiple name="images[]" />
   <button type="submit">Send</button>
 </form>
 </template>
