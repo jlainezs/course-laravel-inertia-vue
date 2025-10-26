d9be54a0a1788699aaa052c6201a3dfeb9b01f70
Pep Lainez <jlainezs@gmail.com>
2025-10-26 11:58:45 +0100

    143 - Seeing which offer is sold

--- resources/js/Pages/Realtor/Show.vue
+++ resources/js/Pages/Realtor/Show.vue
@@ -9,20 +9,23 @@
         No offers
       </div>
     </Box>
-    <div v-else class="md:col-span-7 item-center">
+    <div v-else class="md:col-span-7 flex flex-col gap-4">
       <Offer
         v-for="offer in listing.offers"
         :offer="offer"
         :listing-price="listing.price"
         :key="offer.id"
-        class="mb-4" />
+      />
     </div>
-    <Box class="md:col-span-5">
-      <template #header>Basic Info</template>
-      <Price :price="listing.price" class="text-2xl font-bold" />
-      <ListingSpace :listing="listing" class="text-lg" />
-      <ListingAddress :listing="listing" class="text-gray-500" />
-    </Box>
+
+    <div class="md:col-span-5">
+      <Box>
+        <template #header>Basic Info</template>
+        <Price :price="listing.price" class="text-2xl font-bold" />
+        <ListingSpace :listing="listing" class="text-lg" />
+        <ListingAddress :listing="listing" class="text-gray-500" />
+      </Box>
+    </div>
   </section>
 </template>
 
