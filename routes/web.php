4b906ebd0e6d4fd99c49ed5a7af590c614c9f55d
Pep Lainez <jlainezs@gmail.com>
2025-10-30 07:52:03 +0100

    162 - Resend verification link

--- routes/web.php
+++ routes/web.php
@@ -47,6 +47,10 @@ Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $requ
     return redirect(route('listing.index'))
         ->with('success', 'Email was verified');
 })->middleware(['auth', 'signed'])->name('verification.verify');
+Route::post('/email/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+    return back()->with('message', 'Verification link sent!');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
 
 Route::resource('user-account', UserAccountController::class)
     ->only(['create', 'store']);
